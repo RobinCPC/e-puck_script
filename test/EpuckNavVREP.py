57a02fe1a3609f88831265e58f46ce8b776d4971
juiyunchang <chang.juiyun@gmail.com>
2016-05-03 17:50:02 -0700

    add commit back (after rebase)

--- test/EpuckNavVREP.py
+++ test/EpuckNavVREP.py
@@ -1,4 +1,29 @@
+# Copyright 2006-2016 Coppelia Robotics GmbH. All rights reserved. 
+# marc@coppeliarobotics.com
+# www.coppeliarobotics.com
+# 
+# -------------------------------------------------------------------
+# THIS FILE IS DISTRIBUTED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTY. THE USER WILL USE IT AT HIS/HER OWN RISK. THE ORIGINAL
+# AUTHORS AND COPPELIA ROBOTICS GMBH WILL NOT BE LIABLE FOR DATA LOSS,
+# DAMAGES, LOSS OF PROFITS OR ANY OTHER KIND OF LOSS WHILE USING OR
+# MISUSING THIS SOFTWARE.
+# 
+# You are free to use/modify/distribute this file for whatever purpose!
+# -------------------------------------------------------------------
+#
+# This file was automatically created for V-REP release V3.3.0 on February 19th 2016
 
+# Make sure to have the server side running in V-REP: 
+# in a child script of a V-REP scene, add following command
+# to be executed just once, at simulation start:
+#
+# simExtRemoteApiStart(19999)
+#
+# then start simulation, and run this program.
+#
+# IMPORTANT: for each successful call to simxStart, there
+# should be a corresponding call to simxFinish at the end!
 
 try:
     import vrep
@@ -45,29 +70,3 @@ else:
     print ('Failed connecting to remote API server')
 print ('Program ended')
 
-# Copyright 2006-2016 Coppelia Robotics GmbH. All rights reserved. 
-# marc@coppeliarobotics.com
-# www.coppeliarobotics.com
-# 
-# -------------------------------------------------------------------
-# THIS FILE IS DISTRIBUTED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
-# WARRANTY. THE USER WILL USE IT AT HIS/HER OWN RISK. THE ORIGINAL
-# AUTHORS AND COPPELIA ROBOTICS GMBH WILL NOT BE LIABLE FOR DATA LOSS,
-# DAMAGES, LOSS OF PROFITS OR ANY OTHER KIND OF LOSS WHILE USING OR
-# MISUSING THIS SOFTWARE.
-# 
-# You are free to use/modify/distribute this file for whatever purpose!
-# -------------------------------------------------------------------
-#
-# This file was automatically created for V-REP release V3.3.0 on February 19th 2016
-
-# Make sure to have the server side running in V-REP: 
-# in a child script of a V-REP scene, add following command
-# to be executed just once, at simulation start:
-#
-# simExtRemoteApiStart(19999)
-#
-# then start simulation, and run this program.
-#
-# IMPORTANT: for each successful call to simxStart, there
-# should be a corresponding call to simxFinish at the end!
