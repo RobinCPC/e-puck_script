e4eefca4b17f035a988212ce9d1adb19f1714a21
RobinChen <chienpinchen@gmail.com>
2016-05-03 17:30:34 -0700

    testing delete comment

--- EpuckNavVREP.py
+++ EpuckNavVREP.py
@@ -11,19 +11,7 @@
 # 
 # You are free to use/modify/distribute this file for whatever purpose!
 # -------------------------------------------------------------------
-#
-# This file was automatically created for V-REP release V3.3.0 on February 19th 2016
 
-# Make sure to have the server side running in V-REP: 
-# in a child script of a V-REP scene, add following command
-# to be executed just once, at simulation start:
-#
-# simExtRemoteApiStart(19999)
-#
-# then start simulation, and run this program.
-#
-# IMPORTANT: for each successful call to simxStart, there
-# should be a corresponding call to simxFinish at the end!
 import math
 
 try:
